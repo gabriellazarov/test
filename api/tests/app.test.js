7191549009c08fb95555187a547b2896e4344d76
Gabriel Lazarov <gabriel.lazarov@gmail.com>
2022-06-30 11:59:34 +0300

    total test command + expanded upon api tests

--- api/tests/app.test.js
+++ api/tests/app.test.js
@@ -9,14 +9,28 @@ describe("Testing the app", () => {
 
   test("GET /products works", async () => {
     const response = await request(app).get("/products");
-    expect(response.body).toBeInstanceOf(Array);
-    expect(response.body[0]).toBeInstanceOf(Object);
+
+    expect(typeof response.body[0].id).toBe("string");
+    expect(typeof response.body[0].appId).toBe("string");
+    expect(typeof response.body[0].inSubscription).toBe("boolean");
+    expect(typeof response.body[0].title).toBe("string");
+    expect(typeof response.body[0].subtitle).toBe("string");
+    expect(typeof response.body[0].description).toBe("string");
+    expect(typeof response.body[0].subscription).toBe("string");
+
     expect(response.statusCode).toBe(200);
   });
 
   test("GET /products/:id/plans works w/ correct id", async () => {
     const response = await request(app).get("/products/trip-trends/plans");
-    expect(response.body).toBeInstanceOf(Array);
+
+    expect(typeof response.body[0].ID).toBe("string");
+    expect(typeof response.body[0]["Plan Name"]).toBe("string");
+    expect(typeof response.body[0]["Billing Cycle"]).toBe("string");
+    expect(typeof response.body[0].Name).toBe("string");
+    expect(typeof response.body[0].Type).toBe("string");
+    expect(typeof response.body[0].Description).toBe("string");
+
     expect(response.statusCode).toBe(200);
   });
 
@@ -29,6 +43,26 @@ describe("Testing the app", () => {
     const response = await request(app)
       .post("/login")
       .send({ username: "latekoc917@qqhow.com", password: "123456" });
+
+    const userInfo = response.body;
+
+    expect(typeof userInfo.id).toBe("string");
+    expect(typeof userInfo.email).toBe("string");
+    expect(typeof userInfo.firstName).toBe("string");
+    expect(typeof userInfo.lastName).toBe("string");
+    expect(typeof userInfo.parents).toBe("string");
+
+    expect(typeof userInfo.accessToken.token).toBe("string");
+    expect(typeof userInfo.accessToken.expiry).toBe("string");
+
+    const preferences = userInfo.preferences.global;
+
+    expect(typeof preferences.locale).toMatch(/^string$|^object$/gm);
+    expect(typeof preferences.dateFormat).toMatch(/^string$|^object$/gm);
+    expect(typeof preferences.timeFormat).toMatch(/^string$|^object$/gm);
+    expect(typeof preferences.timeZone).toMatch(/^string$|^object$/gm);
+    expect(typeof preferences.units).toMatch(/^string$|^object$/gm);
+
     expect(response.statusCode).toBe(200);
   });
 
